262379ef60af737a03fac1ded84ead036db4ca40
Eddie Silveira <edgar.silveira@esignlive.com>
2018-04-29 14:50:38 -0400

    added long lights and action buttons

--- bccommands.js
+++ bccommands.js
@@ -16,15 +16,13 @@
                 if (tok.get("represents")) {
                 
                     characters.forEach(function(chr) {
-                    if (tok.get("represents") === chr.get("_id")) {
-                       
-                        var controlledBy = chr.get("controlledby");
-                        var control = controlledBy.split(",");
-                        if (control.length > 1) {
-                            //log(`name:${tok.get("name")},  controlledBy:${controlledBy}`);
-                            playerTokens.push(tok);
+                        if (tok.get("represents") === chr.get("_id")) {
+                            var controlledBy = chr.get("controlledby");
+                            var control = controlledBy.split(",");
+                            if (control.length > 1) {
+                                playerTokens.push(tok);
+                            }
                         }
-                    }
                 });
             }  
             
@@ -38,19 +36,16 @@
             light_radius: radius,
             light_dimradius: dim
         });
+        log(`${token.get('name')} ${radius} ${dim}`)
      };
      
      const handleChatInput = function(msg) {
          if (msg.type !== 'api' || !playerIsGM(msg.playerid) ) return;
          
          const args = msg.content.split(/\s/);
+         
          //if(msg.content.indexOf("!sil") !== -1) {
          if(args[0] !== '!sil') { return; }
-         
-         //log(args);
-         
-         var playerTokens = getPlayerTokens();
-         //log(playerTokens);
          
          //set the lights strength
          var radius = 10;
@@ -59,19 +54,25 @@
             case 'lon' : {
                 radius = 10;  dim = 1;
             } break;
+            case 'long' : {
+                radius = 20;  dim = 1;
+            } break;
             case 'loff' : {
                 radius = 0;  dim = 0;
             } break;
             case 'lself' : {
                 radius = 0.1;  dim = 0.1;
             } break;
+            default : {
+                buttonCommands = `[Lights On](!sil lon) [Lights Off](!sil loff) [Lights Long](!sil long) [Lights Self](!sil lself)`;
+                sendChat('',`/w ${msg.who} ${buttonCommands}`);
+                return;
+            }
          }
-         //log(`${radius} ${dim} ${playerTokens.length}`)
+         
+         var playerTokens = getPlayerTokens();
          playerTokens.forEach( token => setLights(token, radius, dim) );
-         
-         
      };
-     
      
      const registerEventHandlers = () => {
          on("chat:message", handleChatInput);
